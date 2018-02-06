18e5d6709e1c95b7bedab651b1645662eb3ae64c
jwon114 <jthgwong@gmail.com>
2018-02-07 09:43:24 +1100

    locations on click events, find reviews by address

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,4 +3,10 @@ class WelcomeController < ApplicationController
 	def index
 	end
 
+	def api_show
+		reviews = Review.where(address: params[:searchAddress])
+		
+		render json: reviews
+	end
+
 end
