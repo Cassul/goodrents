91bedab68150d539b748e66fb309e32cc8a5e2ea
Cassul <aysa.garden@gmail.com>
2018-02-06 13:50:58 +1100

    database and tables created

--- db/migrate/20180205222945_create_reviews.rb
+++ db/migrate/20180205222945_create_reviews.rb
@@ -1,7 +1,14 @@
 class CreateReviews < ActiveRecord::Migration[5.1]
   def change
     create_table :reviews do |t|
-
+      t.string :address
+      t.string :suburb
+      t.references :user, foreign_key: true
+      t.integer :utilities
+      t.integer :landlord
+      t.integer :neighbours
+      t.integer :neighbourhood
+      t.text :comment
       t.timestamps
     end
   end
