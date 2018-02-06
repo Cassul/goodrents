5082ed0421d1aff960a7818cc8e73e145bdf0117
Vivien <vivien@Viviens-MacBook-Pro.local>
2018-02-06 17:15:12 +1100

    added login/logout and sign up functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+
+  def new
+  end
+
+  def login
+  end
+
+  def create
+    user=User.new
+    user.username=params[:user_name]
+    user.email=params[:email]
+    user.password=params[:password]
+    user.save
+    session[:user_id]=user.id
+    session[:user_name]=user.username
+    property=Review.new
+    property.address=params[:address]
+    property.suburb=params[:suburb]
+    property.user_id=session[:user_id]
+    property.utilities=params[:utility]
+    property.landlord=params[:landlord]
+    property.neighbours=params[:neighbours]
+    property.neighbourhood=params[:neighbourhood]
+    property.comment=params[:comment]
+    if property.save
+      redirect_to '/'
+    else
+      render :new
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  # has_many:reviews
+  # has_many:messages
+  # has_many:comments
+  has_secure_password
 end
