54c2135188d1037cd5ced36ca11df619db063b04
Cassul <aysa.garden@gmail.com>
2018-02-07 13:03:09 +1100

    Reviews, comments, show and destroy added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,10 +5,17 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new
     @comment.body = params[:body]
-    @comment.user_id = @current_user.id
-    @comment.review_id = params['hint']
-    @comment.save
-    flash.notice = "comment added"
-    redirect_to hint_path(@comment.hint_id)
+    @comment.user_id = params[:user_id]
+    @comment.review_id = params[:review]
+    if @comment.save
+      redirect_to review_path(@comment.review_id)
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params['id'])
+    if @comment.destroy
+      redirect_to review_path(@comment.review_id)
+    end
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,23 @@
 class ReviewsController < ApplicationController
 include SessionsHelper
+  
+  def new
+  end
 
-def show 
-  @review = Review.find(params[:id])
-  @comments = Comment.where(review_id: params[:id])
-end
+  def show 
+    # byebug
+    @review = Review.find(params[:id])
+    @comments = Comment.where(review_id: params[:id])
+  end
+
+  def destroy
+    if logged_in? && @current_user.id === Review.find(params[:id])[:user_id]
+      @review = review.destroy(params[:id])
+      redirect_to hints_path
+    else 
+      redirect_to hint_path
+    end
+  end
 
 
 end
